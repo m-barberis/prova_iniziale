dcf0d3e2d6988eaafc9b932242a32f7152305ada
Matteo Barberis <matteo25b@gmail.com>
2024-02-26 18:47:12 +0100

    Non so se ho cambiato qualcosa

--- stupid_math.c
+++ stupid_math.c
@@ -1,5 +1,13 @@
 #include "stupid_math.h"
 
 float mean(int a, int b) {
-		return (a + b)/2;
+		return (float)(a + b)/(float)2;
+}
+
+complex_float_t complex_mean(complex_int_t a, complex_int_t b) {
+	complex_float_t res = {0, 0};
+	res.re = (a.re + b.re)/2;
+	res.im = (a.im + b.im)/2;
+
+	return res;
 }
